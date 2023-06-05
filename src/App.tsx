298aad7ef101eb7485a2bf910fef6cfd122629fd
Mike Tagg <mdtagg91@gmail.com>
2023-06-05 14:13:22 -0700

    added boilerplate for editor and previewer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import Editor from "./components/Editor"
+import Previewer from "./components/Previewer"
 
 function App() {
 
-  return (
-    <>
-      <textarea id="editor"></textarea>
-    </>
-  )
-}
-
-export default App
+    return (
+      <main className="">
+        <Editor/>
+        <Previewer/>
+      </main>
+    )
+  }
+  
+  export default App
+  
---
+++ src/components/Previewer/index.tsx
@@ -0,0 +1,9 @@
+
+
+const Previewer = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Previewer
